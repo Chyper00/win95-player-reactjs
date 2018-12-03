2294a9ff180816df69938c96ca28be50b1687576
Diego <diego.rxdata@gmail.com>
2018-12-03 21:22:57 -0200

    Updates

---
+++ precache-manifest.5885b880005b01b23066787aeb70264d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9686cb7dc9e29147f0f97ff7df069aa0",
+    "url": "/win95-player-reactjs/static/media/ms-sans-serif.9686cb7d.ttf"
+  },
+  {
+    "revision": "c5a5dc20bfd02d2338103c99e6dc0e10",
+    "url": "/win95-player-reactjs/static/media/cd-player.c5a5dc20.png"
+  },
+  {
+    "revision": "35781ead8e64dbcf346e",
+    "url": "/win95-player-reactjs/static/js/runtime~main.35781ead.js"
+  },
+  {
+    "revision": "e20e97fa5052d1863444",
+    "url": "/win95-player-reactjs/static/js/main.e20e97fa.chunk.js"
+  },
+  {
+    "revision": "6538844106dec5665aaf",
+    "url": "/win95-player-reactjs/static/js/1.65388441.chunk.js"
+  },
+  {
+    "revision": "e20e97fa5052d1863444",
+    "url": "/win95-player-reactjs/static/css/main.d4545429.chunk.css"
+  },
+  {
+    "revision": "d59a3c38d898ac52986ba3628908e183",
+    "url": "/win95-player-reactjs/index.html"
+  }
+];
